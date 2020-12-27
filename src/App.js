a1d6eeaaafa1f990f53a6772867f486e8cbf977c
nithesh712 <57995452+nithesh712@users.noreply.github.com>
2020-12-27 10:31:48 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-function App() {
+import Alert from "./components/layouts/Alert";
+import Navbar from "./components/layouts/Navbar";
+import About from "./components/pages/About";
+import User from "./components/users/User";
+import Home from "./components/pages/Home";
+import NotFound from "./components/pages/NotFound";
+
+import GithubState from "./context/github/GithubState";
+import AlertState from "./context/alert/AlertState";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GithubState>
+      <AlertState>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <div className="container">
+              <Alert />
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+                <Route exact path="/user/:login" component={User} />
+                <Route component={NotFound} />
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </AlertState>
+    </GithubState>
   );
-}
+};
 
 export default App;
---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <>
+      <h1>About this App</h1>
+      <p>App to search github users</p>
+      <p>Its the newly created app</p>
+    </>
+  );
+};
+
+export default About;
